01d64cb7b037d89549e208a95b3685b52d0e2cf3
Silthus <michael@reichenbach.in>
2022-06-11 20:00:23 +0200

    feat(arrays): slice away

--- arrays/sum.go
+++ arrays/sum.go
@@ -6,3 +6,22 @@ func Sum(numbers []int) (sum int) {
 	}
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (tailSums []int) {
+	for _, numbers := range numbersToSum {
+		if len(numbers) < 1 {
+			tailSums = append(tailSums, 0)
+		} else {
+			tail := numbers[1:]
+			tailSums = append(tailSums, Sum(tail))
+		}
+	}
+	return
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -8,3 +8,18 @@ import (
 func TestSum(t *testing.T) {
 	assert.Equal(t, 10, Sum([]int{2, 4, 4}))
 }
+
+func TestSumAll(t *testing.T) {
+	assert.Equal(t, []int{3, 9}, SumAll([]int{1, 2}, []int{0, 9}))
+}
+
+func TestSumAllTails(t *testing.T) {
+	assert.Equal(t, []int{
+		2, 5, 0, 0,
+	}, SumAllTails(
+		[]int{1, 2},
+		[]int{1, 2, 3},
+		[]int{2},
+		[]int{},
+	))
+}
