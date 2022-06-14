2fb6506d7b5dce4ec3ab7dcb533b018b18f6770f
Silthus <michael@reichenbach.in>
2022-06-14 20:49:45 +0200

    feat(dictionary): finish maps exercise

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -1,40 +1,62 @@
 package maps
 
 import (
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 	"testing"
 )
 
-var dictionary = Dictionary{
-	"test": "just a test",
+type DictionaryTestSuite struct {
+	suite.Suite
+	dictionary Dictionary
 }
 
-func TestSearch(t *testing.T) {
-	t.Run("known word", func(t *testing.T) {
-		assertDefinition(t, "test", "just a test")
-	})
-	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		assert.ErrorIs(t, err, ErrNotFound)
-	})
+func (s *DictionaryTestSuite) SetupTest() {
+	s.dictionary = Dictionary{
+		"test": "just a test",
+	}
 }
 
-func TestAdd(t *testing.T) {
-	t.Run("new definition", func(t *testing.T) {
-		err := dictionary.Add("foo", "bar")
-		assert.NoError(t, err)
-		assertDefinition(t, "foo", "bar")
+func (s *DictionaryTestSuite) TestSearch() {
+	s.Run("known word", func() {
+		s.assertDefinition("test", "just a test")
 	})
-	t.Run("existing definition throws error", func(t *testing.T) {
-		err := dictionary.Add("test", "should fail")
-		assert.ErrorIs(t, err, ErrWordExists)
-		assertDefinition(t, "test", "just a test")
+	s.Run("unknown word", func() {
+		_, err := s.dictionary.Search("unknown")
+		s.ErrorIs(err, ErrNotFound)
 	})
 }
 
-func assertDefinition(t *testing.T, word, expectedResult string) {
-	t.Helper()
-	result, err := dictionary.Search(word)
-	assert.NoError(t, err)
-	assert.Equal(t, expectedResult, result)
+func (s *DictionaryTestSuite) TestAdd() {
+	s.Run("new definition", func() {
+		err := s.dictionary.Add("foo", "bar")
+		s.NoError(err)
+		s.assertDefinition("foo", "bar")
+	})
+	s.Run("existing definition throws error", func() {
+		err := s.dictionary.Add("test", "should fail")
+		s.ErrorIs(err, ErrWordExists)
+		s.assertDefinition("test", "just a test")
+	})
+}
+
+func (s *DictionaryTestSuite) TestUpdate() {
+	s.Run("new definition is added", func() {
+		s.dictionary.Update("foo", "bar")
+		s.assertDefinition("foo", "bar")
+	})
+	s.Run("existing definition is updated", func() {
+		s.dictionary.Update("test", "updated test")
+		s.assertDefinition("test", "updated test")
+	})
+}
+
+func (s *DictionaryTestSuite) assertDefinition(word, expectedResult string) {
+	s.T().Helper()
+	result, err := s.dictionary.Search(word)
+	s.NoError(err)
+	s.Equal(expectedResult, result)
+}
+
+func TestWalletTestSuite(t *testing.T) {
+	suite.Run(t, new(DictionaryTestSuite))
 }
